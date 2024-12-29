39d41be45a0b9c07a6e6146d9207dea6026122cb
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2024-12-28 19:33:34 -0500

    make ticket details link more accessible

--- src/features/ticket/components/ticket-item.tsx
+++ src/features/ticket/components/ticket-item.tsx
@@ -15,10 +15,13 @@ type TicketItemProps = {
 };
 
 export function TicketItem({ ticket, isDetail }: TicketItemProps) {
-  const detailButton = (
+  const ticketDetailsLink = (
     <Button variant="outline" size="icon" asChild>
-      <Link href={ticketPath(ticket.id)}>
-        <LucideSquareArrowOutUpRight className="h-4 w-4" />
+      <Link
+        href={ticketPath(ticket.id)}
+        aria-label={`View details for ticket ${ticket.title}`}
+      >
+        <LucideSquareArrowOutUpRight className="h-4 w-4" aria-hidden />
       </Link>
     </Button>
   );
@@ -35,7 +38,7 @@ export function TicketItem({ ticket, isDetail }: TicketItemProps) {
           <CardTitle className="flex items-center gap-x-2">
             <span aria-hidden>{TICKET_ICONS[ticket.status]}</span>
             <h3 className="truncate">{ticket.title}</h3>
-            <p className="sr-only">{`Ticket status is: ${ticket.status}`}</p>
+            <p className="sr-only">{`Ticket status is ${ticket.status}`}</p>
           </CardTitle>
         </CardHeader>
         <CardContent>
@@ -49,7 +52,9 @@ export function TicketItem({ ticket, isDetail }: TicketItemProps) {
         </CardContent>
       </Card>
 
-      {!isDetail && <div className="flex flex-col gap-y-1">{detailButton}</div>}
+      {!isDetail && (
+        <div className="flex flex-col gap-y-1">{ticketDetailsLink}</div>
+      )}
     </div>
   );
 }

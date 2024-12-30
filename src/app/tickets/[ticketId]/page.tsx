7598f94c63de05f8a513c2a924f24e5f1eb8d0a4
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2024-12-29 19:16:38 -0500

    prettier

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -6,6 +6,7 @@ import { buttonVariants } from '@/components/ui/button';
 
 import { TicketItem } from '@/features/ticket/components/ticket-item';
 import { getTicket } from '@/features/ticket/queries/get-ticket';
+
 import { ticketsPath } from '@/paths';
 
 export const metadata: Metadata = {

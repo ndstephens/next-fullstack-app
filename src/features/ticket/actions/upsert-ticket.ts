ba5d824321af4374910853a5908d207c3b77475f
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2025-01-04 16:25:34 -0500

    install zod and use it to validate the upsert form data, wrapped in a try/catch

--- src/features/ticket/actions/upsert-ticket.ts
+++ src/features/ticket/actions/upsert-ticket.ts
@@ -2,29 +2,40 @@
 
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { z } from 'zod';
 
 import { prisma } from '@/lib/prisma';
 
 import { ticketPath, ticketsPath } from '@/paths';
+
+const upsertTicketSchema = z.object({
+  title: z.string().min(1).max(191),
+  content: z.string().min(1).max(1024),
+});
 
 export const upsertTicket = async (
   id: string | undefined,
   _actionState: { message: string },
   formData: FormData,
 ) => {
-  const data = {
-    title: formData.get('title') as string,
-    content: formData.get('content') as string,
-  };
+  try {
+    const data = upsertTicketSchema.parse({
+      title: formData.get('title'),
+      content: formData.get('content'),
+    });
 
-  // TODO: add a try/catch block to handle errors ??
-  await prisma.ticket.upsert({
-    where: {
-      id: id || '',
-    },
-    update: data,
-    create: data,
-  });
+    await prisma.ticket.upsert({
+      where: {
+        id: id || '',
+      },
+      update: data,
+      create: data,
+    });
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return { message: 'Something went wrong' };
+  }
 
   revalidatePath(ticketsPath());
 
